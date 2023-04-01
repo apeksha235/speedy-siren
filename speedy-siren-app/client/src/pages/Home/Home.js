173b233e0bf3cfd560eab9159593c07d051ab8c3
overthinker910 <shreya.khetan@somaiya.edu>
2023-04-02 01:31:18 +0530

    changes

--- speedy-siren-app/client/src/pages/Home/Home.js
+++ speedy-siren-app/client/src/pages/Home/Home.js
@@ -89,24 +89,7 @@ To further expedite the ambulance's arrival, the traffic signal system prioritiz
             </div>
             <div className="col-md-6 col-lg-2 mx-auto footer_col">
               <div className="footer_link_box">
-                <h4>Links</h4>
-                <div className="footer_links">
-                  <Link className="" to="/">
-                    Home
-                  </Link>
-                  <Link className="active" to="/about">
-                    About
-                  </Link>
-                  <Link className="" to="/departments">
-                    Departments
-                  </Link>
-                  <Link className="" to="/doctors">
-                    Doctors
-                  </Link>
-                  <Link className="" to="/contact">
-                    Contact Us
-                  </Link>
-                </div>
+                
               </div>
             </div>
             <div className="col-md-6 col-lg-3 footer_col">
