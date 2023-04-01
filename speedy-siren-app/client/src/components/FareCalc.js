8792f11fe61332ba03275874c4626f03215f9774
Apeksha Hegde <apeksha05hegde@gmail.com>
2023-04-01 23:12:41 +0530

    fare calculator

--- speedy-siren-app/client/src/components/FareCalc.js
+++ speedy-siren-app/client/src/components/FareCalc.js
@@ -1,78 +1,79 @@
 import React,{useState} from 'react';
 import './style.css';
+import intercity from '../images/inter.gif'
+import axios from 'axios'
 
 const FareCalc = () => {
-    const [row, setRow] = useState('');
-    const [column, setColumn] = useState('');
-    const handleRowChange = (e) => {
-        setRow(e.target.value);
+    const [row1, setRow1] = useState('');
+    const [column1, setColumn1] = useState('');
+    const [displayedDistance, setDisplayedDistance] = useState(null);
+    const handleRowChange1 = (e) => {
+        setRow1(e.target.value);
       };
       
-      const handleColumnChange = (e) => {
-        setColumn(e.target.value);
+      const handleColumnChange1 = (e) => {
+        setColumn1(e.target.value);
+      };
+
+      const calculate = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:3002/find-hospital', {
+          row: parseInt(row1),
+          column: parseInt(column1),
+        });
+        const { distance } = response.data;
+         console.log("RESPONSE!!!!!!", distance)
+         setDisplayedDistance(distance)
       };
   return (
-    <section>
-      <div className="grid-flex">
-        <div className="col col-image" style={{ backgroundImage: 'url(airmed.jpg)' }} />
-        <div className="col col-text">
-          <div className="Aligner-item">
-            <h1 style={{ color: '#0055b3' }}>Intra City Fare Calculator</h1>
-            <p>This calculator helps in giving an estimate for intra city ambulance need</p>
-            <div className="input-row">
-            <label htmlFor="inp" className="inp">
-              <input type="text" id="inp" placeholder="&nbsp;" />
-              <span className="label">To(City)</span>
-              <form>
-      <div class="user-box">
-        <label>
-          Row:
-        </label>
-        <input type="number" value={row} onChange={handleRowChange} />
-        </div>
-        <div class="user-box">
-        <label>
-          Column:
-        </label>
-        <input type="number" value={column} onChange={handleColumnChange} />
-        </div>
-      </form>
-              <span className="focus-bg"></span>
-            </label>
-            <br />
-            <br />
-            <label htmlFor="inp" className="inp">
-              <input type="text" id="inp" placeholder="&nbsp;" />
+<section>
+  <div className="grid-flex">
+    <img src={intercity} className="col col-image"></img>
+    <div className="col col-text">
+      <div className="Aligner-item">
+        <h1 style={{ color: '#0055b3', marginTop:'200px' }}>Intra City Fare Calculator</h1>
+        <p>This calculator helps in giving an estimate for intra city ambulance need</p>
+        <form>
+          <div className="input-group">
+            <div className="label-group">
               <span className="label">From</span>
-              <form>
-      <div class="user-box">
-        <label>
-          Row:
-        </label>
-        <input type="number" value={row} onChange={handleRowChange} />
-        </div>
-        <div class="user-box">
-        <label>
-          Column:
-        </label>
-        <input type="number" value={column} onChange={handleColumnChange} />
-        </div>
-      </form>
-              <span className="focus-bg"></span>
-            </label>
-            <br />
-            <br />
-            <br />
             </div>
-            <div className="container">
-              <button className="button-30" role="button">
-                Click here
-              </button>
+            <div className="user-box">
+              <label htmlFor="inp-to-row" className="inp">
+                <input onChange={handleRowChange1} value={row1} type="text" id="inp-to-row" placeholder="&nbsp;" />
+                <span className="label">Row</span>
+              </label>
+            </div>
+            <div className="user-box">
+              <label htmlFor="inp-to-col" className="inp">
+                <input onChange={handleColumnChange1} value={column1} type="text" id="inp-to-col" placeholder="&nbsp;" />
+                <span className="label">Column</span>
+              </label>
             </div>
           </div>
+        </form>
+        <br />
+        <br />
+        <br />
+        <div className="container">
+          <button onClick={calculate} className="button-30" role="button">
+            Click here
+          </button>
+          <br>
+          </br>
+          <br></br>
+          {displayedDistance !== null && (
+          <div>
+            <p>Cost from nearest hospital: Rs.{displayedDistance*70}</p>
           </div>
+        )}
+
+        </div>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
+
   );
 };
 
